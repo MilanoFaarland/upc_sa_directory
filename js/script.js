21b559c746175ba5ae1ae668b4af487601b4eb45
Milano <milanofaarland@gmail.com>
2025-09-29 18:31:51 +0200

    Base project created with required components

---
+++ js/script.js
@@ -0,0 +1,499 @@
+// Eastern Cape,
+// Free State,
+// Gauteng,
+// KwaZulu-Natal,
+// Limpopo, Mpumalanga,
+// Northern Cape,
+// North West,
+// Western Cape,
+// import { places } from "./dataNew.js";
+
+const places = [
+  {
+    churchYearEstablished: null,
+    churchName: "New Life Tebernacle",
+    churchAddress: "355 Flower St, Capital Park, Pretoria",
+    churchCity: "",
+    churchProvince: "",
+    churchPostalCode: "0084",
+    churchLatitude: -25.727543523157145,
+    churchLongitude: 28.197431286506962,
+    churchServiceTimes: ["", ""],
+
+    pastorFirstLastName: "Marlin Johnson",
+    pastorPhone: "0744752365",
+    pastorEmail: "marlin@newlifetabernacle.co.za",
+  },
+  {
+    churchYearEstablished: null,
+    churchName: "Praise House Family Worship Center",
+    churchAddress: "5809 Phelindaba St, Roodekop, Germiston",
+    churchCity: "Germiston",
+    churchProvince: "",
+    churchPostalCode: "1401",
+    churchLatitude: -26.31450138188391,
+    churchLongitude: 28.210039299999405,
+    churchServiceTimes: ["", ""],
+
+    pastorFirstLastName: "Elphus Mushwana",
+    pastorPhone: "0720672665",
+    pastorEmail: "elphus0421@gmail.com",
+  },
+  {
+    churchYearEstablished: null,
+    churchName: "Light and Hope Community Church",
+    churchAddress: "Corner of Kritzenger & Peit Joubert SE2, Vanderbijlpark",
+    churchCity: "",
+    churchProvince: "",
+    churchPostalCode: "1911",
+    churchLatitude: -26.713679417485256,
+    churchLongitude: 27.84437472239054,
+    churchServiceTimes: ["", ""],
+
+    pastorFirstLastName: "Delme Veldschoen",
+    pastorPhone: "0765170976",
+    pastorEmail: "dveldschoen21@gmail.com",
+  },
+  {
+    churchYearEstablished: null,
+    churchName: "The Ark of God",
+    churchAddress: "318 Skeleton Coast Street, Maokeng Section, Tembisa",
+    churchCity: "",
+    churchProvince: "",
+    churchPostalCode: "1632",
+    churchLatitude: -25.996646094495546,
+    churchLongitude: 28.220690344801625,
+    churchServiceTimes: ["", ""],
+
+    pastorFirstLastName: "Reginald Manthata",
+    pastorPhone: "0834322523",
+    pastorEmail: "rsmanthata@gmail.com",
+  },
+  {
+    churchYearEstablished: null,
+    churchName: "God's Tabernacle Community Church",
+    churchAddress: "48 Kiaat Street Eldorado Park Ext 3 Johannesburg South",
+    churchCity: "",
+    churchProvince: "",
+    churchPostalCode: "1811",
+    churchLatitude: -26.29748700076623,
+    churchLongitude: 27.917551034490327,
+    churchServiceTimes: ["", ""],
+
+    pastorFirstLastName: "Lionel Adams",
+    pastorPhone: "0614272789",
+    pastorEmail: "pastoradams66@gmail.com",
+  },
+  {
+    churchYearEstablished: null,
+    churchName: "UPC Johannesburg",
+    churchAddress: "Nugget Square, 12 Nugget St",
+    churchCity: "Johannesburg",
+    churchProvince: "",
+    churchPostalCode: "2001",
+    churchLatitude: -26.20731935043589,
+    churchLongitude: 28.05339239880224,
+    churchServiceTimes: ["", ""],
+
+    pastorFirstLastName: "Clifford Nkosi",
+    pastorPhone: "0823410957",
+    pastorEmail: "clifford@cknsecurity.co.za",
+  },
+  {
+    churchYearEstablished: null,
+    churchName: "Truth Tabernacle ",
+    churchAddress: "8th Ave, & Percy Street, Mid-Ennerdale",
+    churchCity: "",
+    churchProvince: "",
+    churchPostalCode: "",
+    churchLatitude: -26.41888794285218,
+    churchLongitude: 27.842177523808786,
+    churchServiceTimes: ["", ""],
+
+    pastorFirstLastName: "Martin Klassen",
+    pastorPhone: "0672453185",
+    pastorEmail: "actsfiles@gmail.com",
+  },
+  {
+    churchYearEstablished: null,
+    churchName: "UPC Eersterust ",
+    churchAddress: "2 Filander Avenue, Eersterust ",
+    churchCity: "",
+    churchProvince: "",
+    churchPostalCode: "",
+    churchLatitude: -25.715185575290967,
+    churchLongitude: 28.304255644178273,
+    churchServiceTimes: ["", ""],
+
+    pastorFirstLastName: "Michael Nawezhi",
+    pastorPhone: "0733780080",
+    pastorEmail: "myknawezhi@gmail.com",
+  },
+  {
+    churchYearEstablished: null,
+    churchName: "UPC Nellmapius ",
+    churchAddress: "6858 Nobelium creasent ",
+    churchCity: "",
+    churchProvince: "",
+    churchPostalCode: "",
+    churchLatitude: -25.730522499256104,
+    churchLongitude: 28.370624746626742,
+    churchServiceTimes: ["", ""],
+
+    pastorFirstLastName: "Isaac Meyer",
+    pastorPhone: "0783272652 ",
+    pastorEmail: "Isaac.vilensky1502@gmail.com",
+  },
+  {
+    churchYearEstablished: null,
+    churchName: "UPC Lotus Gardens ",
+    churchAddress:
+      "Extension 5, West Fort, Sica street, Lotus Gardens, Pretoria, ",
+    churchCity: "",
+    churchProvince: "",
+    churchPostalCode: "0008",
+    churchLatitude: -25.74414029335425,
+    churchLongitude: 28.093645164629642,
+    churchServiceTimes: ["", ""],
+
+    pastorFirstLastName: "Benneth Mashele",
+    pastorPhone: "0824069339",
+    pastorEmail: "drbmashele@gmail.com",
+  },
+  {
+    churchYearEstablished: null,
+    churchName: "The Chief Cornerstone ",
+    churchAddress: "642 Olivia St, Garsfontein, Pretoria ",
+    churchCity: "",
+    churchProvince: "",
+    churchPostalCode: "0081",
+    churchLatitude: -25.79522234622447,
+    churchLongitude: 28.309312968740187,
+    churchServiceTimes: ["", ""],
+
+    pastorFirstLastName: "Dalton Mkhombo ",
+    pastorPhone: "0798071538 ",
+    pastorEmail: "Mkhombod@gmail.com",
+  },
+  {
+    churchYearEstablished: null,
+    churchName: "UPC Bronkhorstspruit ",
+    churchAddress: "59 Fiddes Street Bronkhorstspruit ",
+    churchCity: "",
+    churchProvince: "",
+    churchPostalCode: "",
+    churchLatitude: -25.81162972724622,
+    churchLongitude: 28.737503142328684,
+    churchServiceTimes: ["", ""],
+
+    pastorFirstLastName: "Allroy Halgryn",
+    pastorPhone: "0723248465",
+    pastorEmail: "allroyhalgryn@gmail.com",
+  },
+  {
+    churchYearEstablished: null,
+    churchName: "United Pentecostal Church of Mamelodi ",
+    churchAddress: "Mamelodi east, Amanda Park",
+    churchCity: "",
+    churchProvince: "",
+    churchPostalCode: "",
+    churchLatitude: null,
+    churchLongitude: null,
+    churchServiceTimes: ["", ""],
+
+    pastorFirstLastName: "O'Neal Peterie",
+    pastorPhone: "0695628626",
+    pastorEmail: "peterieoneal@gmail.com",
+  },
+  {
+    churchYearEstablished: null,
+    churchName: "Pentecostals of Windmilpark",
+    churchAddress: "East Central Next Windmil Park Secondary School",
+    churchCity: "",
+    churchProvince: "",
+    churchPostalCode: "",
+    churchLatitude: null,
+    churchLongitude: null,
+    churchServiceTimes: ["", ""],
+
+    pastorFirstLastName: "Emmanuel Seema",
+    pastorPhone: "0832804264",
+    pastorEmail: "emmanuel.seema@flytaag.com",
+  },
+  {
+    churchYearEstablished: null,
+    churchName: "Ark of Germiston ",
+    churchAddress: "Victoria and Knox streets ",
+    churchCity: "",
+    churchProvince: "",
+    churchPostalCode: "",
+    churchLatitude: -26.2027320115782,
+    churchLongitude: 28.16120563885213,
+    churchServiceTimes: ["", ""],
+
+    pastorFirstLastName: "Themba Ndiweni",
+    pastorPhone: "0827074731",
+    pastorEmail: "tndiweni.ndix@gmail.com",
+  },
+  {
+    churchYearEstablished: null,
+    churchName: "Orange Grove UPC",
+    churchAddress: "Cnr 11th st and Louis Botha, Orange Groove ",
+    churchCity: "",
+    churchProvince: "",
+    churchPostalCode: "",
+    churchLatitude: -26.14338497452511,
+    churchLongitude: 28.086495534125465,
+
+    churchServiceTimes: ["", ""],
+
+    pastorFirstLastName: "Peter Ndhlovu",
+    pastorPhone: "0716463521",
+    pastorEmail: "jb.ndlovu56@yahoo.com",
+  },
+  {
+    churchYearEstablished: null,
+    churchName: "UPC Dwelling Moshongoville",
+    churchAddress: "Extension 12, M22, Moshongo ",
+    churchCity: "",
+    churchProvince: "",
+    churchPostalCode: "",
+    churchLatitude: null,
+    churchLongitude: null,
+    churchServiceTimes: ["", ""],
+
+    pastorFirstLastName: "Julia Manzini",
+    pastorPhone: "0727567987",
+    pastorEmail: "Manzinij80@gmil.com",
+  },
+  // {
+  //   churchYearEstablished: null,
+  //   churchName: "",
+  //   churchAddress: "",
+  //   churchCity: "",
+  //   churchProvince: "",
+  //   churchPostalCode: "",
+  //   churchLatitude: null,
+  //   churchLongitude: null,
+  //   churchServiceTimes: ["", ""],
+
+  //   pastorFirstLastName: "",
+  //   pastorPhone: "",
+  //   pastorEmail: "",
+  // },
+  // {
+  //   churchYearEstablished: null,
+  //   churchName: "",
+  //   churchAddress: "",
+  //   churchCity: "",
+  //   churchProvince: "",
+  //   churchPostalCode: "",
+  //   churchLatitude: null,
+  //   churchLongitude: null,
+  //   churchServiceTimes: ["", ""],
+
+  //   pastorFirstLastName: "",
+  //   pastorPhone: "",
+  //   pastorEmail: "",
+  // },
+  // {
+  //   churchYearEstablished: null,
+  //   churchName: "",
+  //   churchAddress: "",
+  //   churchCity: "",
+  //   churchProvince: "",
+  //   churchPostalCode: "",
+  //   churchLatitude: null,
+  //   churchLongitude: null,
+  //   churchServiceTimes: ["", ""],
+
+  //   pastorFirstLastName: "",
+  //   pastorPhone: "",
+  //   pastorEmail: "",
+  // },
+  // {
+  //   churchYearEstablished: null,
+  //   churchName: "",
+  //   churchAddress: "",
+  //   churchCity: "",
+  //   churchProvince: "",
+  //   churchPostalCode: "",
+  //   churchLatitude: null,
+  //   churchLongitude: null,
+  //   churchServiceTimes: ["", ""],
+
+  //   pastorFirstLastName: "",
+  //   pastorPhone: "",
+  //   pastorEmail: "",
+  // },
+  // {
+  //   churchYearEstablished: null,
+  //   churchName: "",
+  //   churchAddress: "",
+  //   churchCity: "",
+  //   churchProvince: "",
+  //   churchPostalCode: "",
+  //   churchLatitude: null,
+  //   churchLongitude: null,
+  //   churchServiceTimes: ["", ""],
+
+  //   pastorFirstLastName: "",
+  //   pastorPhone: "",
+  //   pastorEmail: "",
+  // },
+  // {
+  //   churchYearEstablished: null,
+  //   churchName: "",
+  //   churchAddress: "",
+  //   churchCity: "",
+  //   churchProvince: "",
+  //   churchPostalCode: "",
+  //   churchLatitude: null,
+  //   churchLongitude: null,
+  //   churchServiceTimes: ["", ""],
+
+  //   pastorFirstLastName: "",
+  //   pastorPhone: "",
+  //   pastorEmail: "",
+  // },
+  // {
+  //   churchYearEstablished: null,
+  //   churchName: "",
+  //   churchAddress: "",
+  //   churchCity: "",
+  //   churchProvince: "",
+  //   churchPostalCode: "",
+  //   churchLatitude: null,
+  //   churchLongitude: null,
+  //   churchServiceTimes: ["", ""],
+
+  //   pastorFirstLastName: "",
+  //   pastorPhone: "",
+  //   pastorEmail: "",
+  // },
+  // {
+  //   churchYearEstablished: null,
+  //   churchName: "",
+  //   churchAddress: "",
+  //   churchCity: "",
+  //   churchProvince: "",
+  //   churchPostalCode: "",
+  //   churchLatitude: null,
+  //   churchLongitude: null,
+  //   churchServiceTimes: ["", ""],
+
+  //   pastorFirstLastName: "",
+  //   pastorPhone: "",
+  //   pastorEmail: "",
+  // },
+];
+
+let currentPage = 1;
+const rowsPerPage = 10;
+let map,
+  markers = [],
+  tableRows = [];
+let filteredPlaces = [...places]; // initially same as all places
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -26, lng: 28 },
+    zoom: 9,
+  });
+  console.log(places);
+  renderTable();
+}
+
+function renderTable() {
+  const tableBody = document.querySelector("#placesTable tbody");
+  tableBody.innerHTML = "";
+
+  // clear old markers
+  markers.forEach((m) => m.setMap(null));
+  markers = [];
+  tableRows = [];
+
+  const start = (currentPage - 1) * rowsPerPage;
+  const end = start + rowsPerPage;
+  const pageItems = filteredPlaces.slice(start, end);
+
+  pageItems.forEach((place, index) => {
+    // Add row to table
+    const row = document.createElement("tr");
+    row.innerHTML = `
+          <td>${place.churchName}</td>
+          <td>${place.churchAddress}</td>
+          <td>${place.pastorFirstLastName}</td>
+          `;
+    tableBody.appendChild(row);
+    tableRows.push(row);
+
+    // Add marker to map
+    const marker = new google.maps.Marker({
+      position: { lat: place.churchLatitude, lng: place.churchLongitude },
+      map: map,
+      title: place.churchName,
+    });
+    markers.push(marker);
+
+    // Row click → highlight marker
+    row.addEventListener("click", () => highlight(index));
+
+    // Marker click → highlight row
+    marker.addListener("click", () => highlight(index));
+  });
+
+  // update page info
+  document.getElementById(
+    "pageInfo"
+  ).textContent = `Page ${currentPage} of ${Math.max(
+    1,
+    Math.ceil(filteredPlaces.length / rowsPerPage)
+  )}`;
+}
+
+function highlight(index) {
+  // Reset all markers and rows
+  markers.forEach((m) => m.setAnimation(null));
+  tableRows.forEach((r) => r.classList.remove("highlight"));
+
+  // Highlight chosen marker and row
+  markers[index].setAnimation(google.maps.Animation.BOUNCE);
+  setTimeout(() => markers[index].setAnimation(null), 1600);
+  tableRows[index].classList.add("highlight");
+
+  // Pan map to the marker
+  map.panTo(markers[index].getPosition());
+  map.setZoom(15);
+}
+
+function nextPage() {
+  if (currentPage * rowsPerPage < filteredPlaces.length) {
+    currentPage++;
+    renderTable();
+  }
+}
+
+function prevPage() {
+  if (currentPage > 1) {
+    currentPage--;
+    renderTable();
+  }
+}
+
+function applyFilter() {
+  const query = document.getElementById("searchBox").value.toLowerCase();
+  filteredPlaces = places.filter((p) =>
+    p.churchName.toLowerCase().includes(query)
+  );
+  currentPage = 1; // reset to first page
+  renderTable();
+}
+
+function displayCurrentYear() {
+  currentYear = document.getElementById("currentYear");
+
+  currentYear = getYear();
+}
+
+window.onload = initMap;
